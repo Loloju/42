f31c79780be7a31ee41aa33d5bd798ac8aeea8fb
odemirel <>
2022-02-17 11:15:53 +0300

    fancy

--- cursus/libft/libft.h
+++ cursus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: odemirel <odemirel@student.42kocaeli.>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 21:33:35 by odemirel          #+#    #+#             */
-/*   Updated: 2022/02/14 16:29:42 by odemirel         ###   ########.fr       */
+/*   Updated: 2022/02/17 10:25:17 by odemirel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,9 @@ int			ft_atoi(const char *s);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 int			ft_strncmp(const char *s_1, const char *s_2, size_t n);
 
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlen(const char *s);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 void		ft_bzero(void *source, size_t n);
 void		*ft_calloc(size_t amount, size_t n);
@@ -64,13 +64,13 @@ char		*ft_itoa(int n);
 
 void		ft_striteri(char *s, void (*f)(unsigned int, char*));
 
-t_list	*ft_lstnew(void *content);
+t_list		*ft_lstnew(void *content);
 void		ft_lstadd_front(t_list **lst, t_list *new);
 int			ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
 void		ft_lstadd_back(t_list **lst, t_list *new);
 void		ft_lstdelone(t_list *lst, void (*del)(void *));
 void		ft_lstclear(t_list **lst, void (*del)(void *));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
