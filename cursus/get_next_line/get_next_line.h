02c00bce3186b45a81373ee8f600372e19e3815f
odemirel <>
2022-02-21 16:44:49 +0300

    unnecessary files

--- cursus/get_next_line/get_next_line.h
+++ cursus/get_next_line/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: odemirel <odemirel@student.42kocaeli.>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 16:45:22 by odemirel          #+#    #+#             */
-/*   Updated: 2022/02/15 09:51:31 by odemirel         ###   ########.fr       */
+/*   Updated: 2022/02/21 09:48:43 by odemirel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-
+char	*get_next_line(int fd);
 
 #endif
