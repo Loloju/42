7e98fe799071b2863ab284019b8798723b3b7ecc
odemirel <>
2022-03-22 16:58:47 +0300

    end of day

--- cursus/ft_printf/ft_printf.c
+++ cursus/ft_printf/ft_printf.c
@@ -6,61 +6,42 @@
 /*   By: odemirel <odemirel@student.42kocaeli.com.t +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 10:48:02 by odemirel          #+#    #+#             */
-/*   Updated: 2022/03/16 14:01:52 by odemirel         ###   ########.fr       */
+/*   Updated: 2022/03/22 16:57:55 by odemirel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-static char	*birlestir(char *s1, const char *s2, int n)
+static int	check_flag(char c, va_list args)
 {
-	char	*s3;
-	int		i;
-	int		j;
+	int	len;
 
-	s3 = malloc(sizeof(char) * (ft_strlen(s1) + n + 1));
-	if (!s3)
-		return (NULL);
-	s3 = ft_strjoin(s3, s1);
-	i = ft_strlen(s3);
-	j = 0;
-	while (s2[j])
-	{
-		s3[i] = s2[j];
-		i++;
-		j++;
-	}
-	s3[i] = '\0';
-	return (s3);
-}
-
-int	check_flag(char c, va_list args, int len)
-{
+	len = 0;
 	if (c == 'c')
-		return (len += ft_putchar_len(va_arg(args, char), len));
+		return (len += ft_putchar_len(va_arg(args, int)));
 	if (c == 's')
-		return (len += ft_putstr_len(va_arg(args, char *), len));
+		return (len += ft_putstr_len(va_arg(args, char *)));
 	if (c == 'p')
-		return (len += ft_putptr_len(va_arg(args, int), len));
-	if (c == 'd' || c == 'i')
-		return (len += ft_itoa_len(va_arg(args, int), len));
+		return (len += ft_putptr_len(va_arg(args, int)));
+	/* if (c == 'd' || c == 'i')
+		return (len += ft_itoa_len(va_arg(args, int)));
 	if (c == 'u')
-		return (len += ft_unsigned_len(va_arg(args, int), len));
+		return (len += ft_unsigned_len(va_arg(args, int)));
 	if (c == 'x' || c == 'X')
-		return (len += ft_puthex_len(va_arg(args, int), len));
-	if (c == '%')
-		return (len += ft_putchar_len('%', len));
+		return (len += ft_puthex_len(va_arg(args, int))); */
+	// if (c == '%')
+	// 	return (len += ft_putchar_len('%'));
 	return (0);
 }
 
 int	ft_printf(const char *str, ...)
 {
 	va_list	valist;
-	int		i;
 	int		len;
-	char	*string;
+	int		i;
 
+	len = 0;
 	i = 0;
 	va_start(valist, str);
 	while (str[i] != '\0')
@@ -72,19 +53,20 @@ int	ft_printf(const char *str, ...)
 		}
 		else
 		{
-			if (str[i + 1] != NULL)
+			if (str[i + 1] != '\0')
 			{
-				len += check_flag(str[i++], valist, len);
-				i++;
+				len += check_flag(str[++i], valist);
 			}
 			else
 				return (-1);
 		}
+		i++;
 	}
+	return (len);
 }
 
 int	main(void)
 {
-	ft_printf("yelloooo", "asd", "zxc", "askfadsf", "asdf");
+	int	a = ft_printf("yelloooo %s\n", "hello there");
 	return (0);
 }
--- cursus/ft_printf/ft_printf.h
+++ cursus/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: odemirel <odemirel@student.42kocaeli.com.t +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 10:37:35 by odemirel          #+#    #+#             */
-/*   Updated: 2022/03/16 15:51:56 by odemirel         ###   ########.fr       */
+/*   Updated: 2022/03/17 13:51:14 by odemirel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@
 
 int	ft_printf(const char *count, ...);
 
-int	ft_putchar_len(va_list args, char c);
-int	ft_putstr_len(va_list args, char *str);
-int	ft_putptr_len(va_list args, unsigned long long ptr);
+int	ft_putchar_len(char arg);
+int	ft_putstr_len(char *args);
+int	ft_putptr_len(unsigned long long arg);
+int	ft_itoa_len(int arg);
+int	ft_unsigned_len(unsigned int arg);
+int	ft_puthex_len(unsigned int args);
 #endif
