7007d15ad8685335e93d2b9cd3af67e60cb69b40
odemirel <>
2022-03-29 17:14:43 +0300

    29/03/2022

--- cursus/ft_printf/ft_printf.c
+++ cursus/ft_printf/ft_printf.c
@@ -6,13 +6,12 @@
 /*   By: odemirel <odemirel@student.42kocaeli.com.t +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 10:48:02 by odemirel          #+#    #+#             */
-/*   Updated: 2022/03/25 14:54:40 by odemirel         ###   ########.fr       */
+/*   Updated: 2022/03/29 12:21:06 by odemirel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
-#include <limits.h>
+
 static int	check_flag(char c, va_list args)
 {
 	int	len;
@@ -26,8 +25,8 @@ static int	check_flag(char c, va_list args)
 		return (len += ft_putptr_len(va_arg(args, unsigned long long)));
 	if (c == 'd' || c == 'i')
 		return (len += ft_itoa_len(va_arg(args, int)));
-	/*if (c == 'u')
-		return (len += ft_unsigned_len(va_arg(args, unsigned int))); */
+	if (c == 'u')
+		return (len += ft_unsigned_len(va_arg(args, unsigned int)));
 	if (c == 'x' || c == 'X')
 		return (len += ft_puthex_len(va_arg(args, unsigned int), c));
 	if (c == '%')
@@ -59,14 +58,21 @@ int	ft_printf(const char *str, ...)
 		}
 		i++;
 	}
+	va_end(valist);
 	return (len);
 }
 
-int	main(void)
+/* int	main(void)
 {
-	int b = 0;
-	int	a = ft_printf("yelloooo %s %c %d %x %x %x %p\n", "hello there is", 'a', 13, 5555, -5555, INT_MAX, &b);
-	int c = printf("yelloooo %s %c %d %x %x %x %p\n", "hello there is", 'a', 13, 5555, -5555, INT_MAX, &b);
+	int	a;
+	int	b;
+	int	c;
+
+	b = 0;
+	a = ft_printf("yelloooo %s %c %d %x %x %x %p %u %u\n",
+			"hello there is", 'a', 13, 5555, -5555, INT_MAX, &b, -20, 2374725);
+	c = printf("yelloooo %s %c %d %x %x %x %p %u %u\n",
+			"hello this is", 'a', 13, 5555, -5555, INT_MAX, &b, -20, 2374725);
 	printf("ft: %d pf: %d", a, c);
 	return (0);
-}
+} */
--- cursus/ft_printf/ft_printf.h
+++ cursus/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: odemirel <odemirel@student.42kocaeli.com.t +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 10:37:35 by odemirel          #+#    #+#             */
-/*   Updated: 2022/03/25 13:52:10 by odemirel         ###   ########.fr       */
+/*   Updated: 2022/03/28 10:45:39 by odemirel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@
 # define HEXA_BIG "0123456789ABCDEF"
 # define HEXA_SMALL "0123456789abcdef"
 
-int	ft_printf(const char *count, ...);
-
-int	ft_putchar_len(char arg);
-int	ft_putstr_len(char *args);
-int	ft_putptr_len(unsigned long long arg);
-int	ft_itoa_len(int arg);
-int	ft_unsigned_len(unsigned int arg);
-int	ft_puthex_len(unsigned int args, char x);
+int		ft_printf(const char *count, ...);
+int		ft_putchar_len(char arg);
+int		ft_putstr_len(char *args);
+int		ft_putptr_len(unsigned long long arg);
+int		ft_itoa_len(int arg);
+int		ft_unsigned_len(unsigned int arg);
+int		ft_puthex_len(unsigned int args, char x);
+char	*ft_utoa(unsigned int nbr);
 #endif
